534c2aa0397b40da4cf8071e0b5184c43be88f46
jochen <jochen@CHCITD1M50FB2.chcn-eb.org>
2016-08-09 16:47:36 -0700

    2_2

--- src/C1arrayandstring/c2_1.java
+++ src/C1arrayandstring/c2_1.java
@@ -55,6 +55,26 @@ public class c2_1 {
         }
     }
     
+    //2_2
+    public static Node nthToLast(Node head, int n){
+        if(head == null || n <1){
+            return null;
+        }
+        Node p1 = head;
+        Node p2 = head;
+        for (int j = 0; j < n - 1; ++j){
+            if(p2 == null){
+                return null;
+            }
+            p2 = p2.next;
+        }
+        while (p2.next != null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1;
+    }
+    
     public static void printNode(Node n){
         Node current = n;// new Node(0);
         //current.next = n;
