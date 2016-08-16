27ac9a7a8cd5a51e3a84ccd458c2656c23127179
jochen <jochen@CHCITD1M50FB2.chcn-eb.org>
2016-08-16 16:45:12 -0700

    create Tree

---
+++ src/C1arrayandstring/c4_1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package C1arrayandstring;
+
+/**
+ *
+ * @author jochen
+ */
+public class c4_1 {
+    public static void main(String[] args){
+    
+    }
+}
+
+class TreeNode{
+    TreeNode left = null;
+    TreeNode right = null;
+}
