e0bfcd72beefc2aab007d554776132c890a11eb9
jochen <jochen@CHCITD1M50FB2.chcn-eb.org>
2016-08-17 15:58:19 -0700

    4_1 done

--- src/C1arrayandstring/c4_1.java
+++ src/C1arrayandstring/c4_1.java
@@ -12,6 +12,28 @@ package C1arrayandstring;
 public class c4_1 {
     public static void main(String[] args){
     
+       
+        
+    }
+    
+    
+    //4_1
+    public static boolean balanceTree(TreeNode root){
+        return (maxDepth(root) - minDepth(root) <= 1);
+    }
+    
+    public static int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+    
+    public static int minDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 }
 
