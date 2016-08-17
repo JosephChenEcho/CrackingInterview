ba44f8e34f94cc3b2b25c1dde3c14e9fe314c609
jochen <jochen@CHCITD1M50FB2.chcn-eb.org>
2016-08-17 16:09:42 -0700

    4_3 done

--- src/C1arrayandstring/c4_1.java
+++ src/C1arrayandstring/c4_1.java
@@ -11,8 +11,26 @@ package C1arrayandstring;
  */
 public class c4_1 {
     public static void main(String[] args){
-    
-       
+        
+        //test case for 4_1
+        /*
+        TreeNode ta = new TreeNode(1);
+        TreeNode tb = new TreeNode(2);
+        TreeNode tc = new TreeNode(3);
+        TreeNode td = new TreeNode(4);
+        TreeNode te = new TreeNode(5);
+        TreeNode tf = new TreeNode(6);
+        TreeNode tg = new TreeNode(7);
+        
+        ta.left = tb;
+        ta.right = tc;
+        tb.left = td;
+        tb.right = te;
+        tc.left = tf;
+        tc.right = tg;
+        
+        System.out.println(balanceTree(ta));
+        */
         
     }
     
@@ -35,9 +53,29 @@ public class c4_1 {
         }
         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
+    
+    //4_3
+    public static TreeNode addToTree(int arr[], int start, int end){
+        if(end < start){
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode n = new TreeNode(arr[mid]);
+        n.left = addToTree(arr, start, mid-1);
+        n.right = addToTree(arr, mid + 1, end);
+        return n;
+    }
+    
+    public static TreeNode createMinBT(int array[]){
+        return addToTree(array, 0, array.length - 1);
+    }
 }
 
 class TreeNode{
     TreeNode left = null;
     TreeNode right = null;
+    Object value;
+    TreeNode(Object _value){
+        this.value = _value;
+    }
 }
